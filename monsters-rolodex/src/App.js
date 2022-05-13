8c5d9ba9d2907ac7aa92ee9823d4df1498cd1ac4
Mateus Conde <devmateuscondefeitosa@gmail.com>
2022-05-13 13:41:14 -0300

    API consuming

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -8,29 +8,33 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: [
-        {
-          name: 'Linda',
-        },
-        {
-          name: 'Frank',
-        },
-        {
-          name: 'Jacky',
-        },
-        {
-          name: 'Lupi',
-        },
-      ]
+      monsters: [],
     };
   }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => this.setState(() => {
+        return {monsters: users}
+      },
+        () => {
+          console.log(this.state);
+        }
+      ));
+  }
+
 
   render() {
     return (
       <div className="App">
         {
           this.state.monsters.map((monster) => {
-            return <h1>{monster.name}</h1>
+            return (
+            <div key={monster.id}> 
+              <h1>{monster.name}</h1> 
+            </div>
+            );
           })
         }
       </div>
