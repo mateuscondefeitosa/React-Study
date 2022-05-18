150a7eae47dca8fde619b045c142404590a14442
Mateus Conde <devmateuscondefeitosa@gmail.com>
2022-05-18 15:24:35 -0300

    react optimizations

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,6 +1,5 @@
 import { Component } from 'react';
 
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -9,13 +8,15 @@ class App extends Component {
 
     this.state = {
       monsters: [],
+      searchField: '',
     };
   }
 
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
-      .then((users) => this.setState(() => {
+      .then((users) => 
+      this.setState(() => {
         return {monsters: users}
       },
         () => {
@@ -24,12 +25,27 @@ class App extends Component {
       ));
   }
 
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return {searchField};
+    });
+  }
 
   render() {
+
+    const { monsters, searchField, } = this.state;
+    const { onSearchChange } = this; 
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+
     return (
       <div className="App">
-        {
-          this.state.monsters.map((monster) => {
+        <input className="search-box" type="search" placeholder='Search monsters' onChange={onSearchChange}/>
+
+        {filteredMonsters.map((monster) => {
             return (
             <div key={monster.id}> 
               <h1>{monster.name}</h1> 
