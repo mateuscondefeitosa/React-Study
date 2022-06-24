5973f3d8311c523e604d89b24fed6edf5077915b
Mateus Conde <devmateuscondefeitosa@gmail.com>
2022-06-24 11:26:49 -0300

    React navbar structure

--- clothingcrown-store-project/src/App.js
+++ clothingcrown-store-project/src/App.js
@@ -1,14 +1,21 @@
-import {Routes, Route} from 'react-router-dom'
+import { Routes, Route } from "react-router-dom";
+
+import Navigation from "./components/routes/navigation/navigation.component";
 import Home from "./components/routes/home/home.component";
 
+const Shop = () => {
+    return <h1>THE SHOP PAGE</h1>;
+};
+
 const App = () => {
-  return (
-    <Routes>
-      <Route path='/' element={<Home />}>
-      
-      </Route>
-    </Routes>
-  )
+    return (
+        <Routes>
+            <Route path="/" element={<Navigation />}>
+                <Route index element={<Home />} />
+                <Route path="shop" element={<Shop />} />
+            </Route>
+        </Routes>
+    );
 };
 
 export default App;
