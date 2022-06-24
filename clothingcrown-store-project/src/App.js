aaffaa93d48401728167886f99bc2ecef7526b85
Mateus Conde <devmateuscondefeitosa@gmail.com>
2022-06-24 15:20:00 -0300

    Firebase pack install + Sign In link

--- clothingcrown-store-project/src/App.js
+++ clothingcrown-store-project/src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 
 import Navigation from "./components/routes/navigation/navigation.component";
 import Home from "./components/routes/home/home.component";
+import SignIn from "./components/routes/sign-in/sign-in.component";
 
 const Shop = () => {
     return <h1>THE SHOP PAGE</h1>;
@@ -13,6 +14,7 @@ const App = () => {
             <Route path="/" element={<Navigation />}>
                 <Route index element={<Home />} />
                 <Route path="shop" element={<Shop />} />
+                <Route path="sign-in" element={<SignIn />} />
             </Route>
         </Routes>
     );
