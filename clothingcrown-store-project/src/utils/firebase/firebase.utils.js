594dafa610dd7b091aca1e566e5f5def19e2b16d
Mateus Conde <devmateuscondefeitosa@gmail.com>
2022-06-25 21:53:20 -0300

    doc, getdoc & setdoc

--- clothingcrown-store-project/src/utils/firebase/firebase.utils.js
+++ clothingcrown-store-project/src/utils/firebase/firebase.utils.js
@@ -1,31 +1,37 @@
-import { initializeApp } from 'firebase/app';
-import { 
-    getAuth,
-    signInWithRedirect,
-    signInWithPopup,
-    GoogleAuthProvider } from 'firebase/auth'
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyB-4Vz-FM2Qul-vHuqNtMs5BiOHzKLqYO0",
-    authDomain: "crwn-clothing-project-db-9f849.firebaseapp.com",
-    projectId: "crwn-clothing-project-db-9f849",
-    storageBucket: "crwn-clothing-project-db-9f849.appspot.com",
-    messagingSenderId: "943954327177",
-    appId: "1:943954327177:web:18cf665eee71dbe961b858"
-  };
-  
+  apiKey: "AIzaSyB-4Vz-FM2Qul-vHuqNtMs5BiOHzKLqYO0",
+  authDomain: "crwn-clothing-project-db-9f849.firebaseapp.com",
+  projectId: "crwn-clothing-project-db-9f849",
+  storageBucket: "crwn-clothing-project-db-9f849.appspot.com",
+  messagingSenderId: "943954327177",
+  appId: "1:943954327177:web:18cf665eee71dbe961b858",
+};
+
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
-  
+
 const provider = new GoogleAuthProvider();
 provider.setCustomParameters({
-    prompt: "select_account"
-}); 
-
+  prompt: "select_account",
+});
 
 export const auth = getAuth();
-export const signInWithGooglePopup = () => signInWithPopup(auth, provider)
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
 
+export const db = getFirestore();
 
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
 
+  const userSnapshot = await getDoc(userDocRef);
+};
