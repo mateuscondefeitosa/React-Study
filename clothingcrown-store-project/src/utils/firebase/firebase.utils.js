75415943558b5543d96d5b36231340918883df40
Mateus Conde <devmateuscondefeitosa@gmail.com>
2022-08-08 22:45:29 -0300

    Creating Google Autentication with firebase

--- clothingcrown-store-project/src/utils/firebase/firebase.utils.js
+++ clothingcrown-store-project/src/utils/firebase/firebase.utils.js
@@ -36,10 +36,22 @@ export const createUserDocumentFromAuth = async (userAuth) => {
   const userSnapshot = await getDoc(userDocRef);
 
   // check if user data not exists
-    // create/set the document with the data from userAuth in my collection
+  // create/set the document with the data from userAuth in my collection
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log("error creating the user", error.message);
+    }
+  }
 
   //check if user data exists
-    //
-
-
+  return userDocRef;
 };
